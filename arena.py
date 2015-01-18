7a0a0e3eb838f0771e1b23790bbf75dd375cfe43
Samuel Taylor <sammy.whythim@gmail.com>
2015-01-18 17:22:06 +0000

    Modified the way that save/load works to accomidate for the arena module and created a breif layout for the arena screen

--- arena.py
+++ arena.py
@@ -1,31 +1,46 @@
 from kivy.uix.widget import Widget
-from kivy.clock import Clock
-from kivy.graphics import *
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.button import Button
 
 from kivy.config import Config
 
+import main
 
 # Sets the initial window size which will be filled with 20 by 20 blocks
 Config.set('graphics', 'width', '850')
 Config.set('graphics', 'height', '780')
 
-layout = GridLayout(cols=40, size_hint_x=None, width=850)
-class Arena_Main(Widget):
+# Sets the background tile type
+type_list = []
 
-
-    # Loop for robot movement and interactions etc
-    # Clock.schedule_interval(self.update, 1.0 / 60.0)
+class ArenaMain(Widget):
+    layout = FloatLayout(size=(800, 780))
     def update(self):
         pass
 
+    def side_buttons(self):
+        rooty = main.Root()
 
-    def start_arena(self):
-        button = Button(text='This is a', font_size=14)
+        def open_press(instance):
+            rooty.set_sender("arena")
+            rooty.show_load()
 
-        button2 = Button(text='test page', font_size=14)
-        layout.add_widget(button)
-        layout.add_widget(button2)
+        # Layout to stack buttons
+        side_btn_layout = StackLayout(size_hint_x=None, width=50)
 
-        return layout
+        # Button to open map file
+        btn_open = Button(text='Open Map', size_hint=(1, .1))
+        btn_open.bind(on_press=open_press)
+
+        side_btn_layout.add_widget(btn_open)
+
+        return side_btn_layout
+
+    def main_arena(self):
+        button = Button(
+            text='Hello world',
+            size_hint=(.5, .25),
+            pos=(50, 20))
+        self.layout.add_widget(button)
+        return self.layout
